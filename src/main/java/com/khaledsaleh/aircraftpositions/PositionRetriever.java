e5cfa2d27f2e39b3a59468dd7a6e15d41dedb56a
Khaled Saleh <th.be03@gmail.com>
2023-10-18 14:09:58 +0200

    WebSocket in back, WebSocket in front

--- src/main/java/com/khaledsaleh/aircraftpositions/PositionRetriever.java
+++ src/main/java/com/khaledsaleh/aircraftpositions/PositionRetriever.java
@@ -3,7 +3,10 @@ package com.khaledsaleh.aircraftpositions;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -11,13 +14,26 @@ import java.util.function.Consumer;
 @Configuration
 public class PositionRetriever {
     private final AircraftRepository repository;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions(){
         return acList -> {
           repository.deleteAll();
           repository.saveAll(acList);
-          repository.findAll().forEach(System.out::println);
+          sendPositions();
         };
+    }
+
+    private void sendPositions(){
+        if (repository.count() > 0){
+            for (WebSocketSession sessionInList : handler.getSessionList()){
+                try {
+                    sessionInList.sendMessage(new TextMessage(repository.findAll().toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
